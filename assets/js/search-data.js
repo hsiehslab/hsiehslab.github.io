c4514ec07d9d5ba20803da68c80634984941c23c
yuchenchang <tommy820531@gmail.com>
2025-11-10 08:28:05 +0000

    Deploying to gh-pages from @ integratedphenomicslab/integratedphenomicslab.github.io@97e26498768d6bad8b77c47a4358f5aec3638096 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "PROJECTS",
           description: "",
           section: "Navigation",
           handler: () => {
