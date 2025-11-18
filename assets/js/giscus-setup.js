0ea3810181c88e674a8d10595ccc5b86f42604cd
yuchenchang <tommy820531@gmail.com>
2025-11-18 09:46:00 +0000

    Deploying to gh-pages from @ hsiehslab/hsiehslab.github.io@c8ba7251aff08857ce5c6efde5908275ff8b7a22 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "integratedphenomicslab/integratedphenomicslab.github.io",
+    "data-repo": "hsiehslab/hsiehslab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
