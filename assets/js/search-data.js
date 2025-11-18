a2933a7d754ce425b4f200a8712f6755130fafaa
yuchenchang <tommy820531@gmail.com>
2025-11-18 11:28:24 +0000

    Deploying to gh-pages from @ hsiehslab/hsiehslab.github.io@ed69a783700da03f895c14cc9fc26e0b1b2ffa60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-dr-tzung-chien-hsieh-is-participating-in-the-6th-bcm-cuhk-zju-joint-symposium-in-clinical-genetics-2025",
+          title: 'Dr. Tzung-Chien Hsieh is participating in the 6th BCM-CUHK-ZJU Joint Symposium in Clinical...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
